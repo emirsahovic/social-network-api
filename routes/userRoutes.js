4f2d2475d88ca3dabb0fb02537a0ff4db9f192f6
emirsahovic <emir.sahovic00@gmail.com>
2022-03-07 21:23:48 +0100

    Name and email validator

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/userController.js";
+import { validateUser } from '../validators/userValidator.js';
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/register', validateUser, registerUser);
+router.post('/login', validateUser, loginUser);
 
 export default router;
---
+++ validators/userValidator.js
@@ -0,0 +1,34 @@
+import { check, validationResult } from 'express-validator';
+
+const validateUser = [
+    check('name')
+        .trim()
+        .escape()
+        .not()
+        .isEmpty()
+        .withMessage('User name can not be empty')
+        .bail()
+        .isLength({ min: 3 })
+        .withMessage('Minimum 3 characters required')
+        .bail(),
+    check('email')
+        .trim()
+        .normalizeEmail()
+        .not()
+        .isEmpty()
+        .withMessage('Invalid email address')
+        .isEmail()
+        .withMessage('Invalid email address')
+        .bail(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const err = errors.array().map(err => err.msg);
+            const msg = err[0];
+            return res.status(422).json({ msg });
+        }
+        next();
+    },
+];
+
+export { validateUser }
