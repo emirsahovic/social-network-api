523d2542b02210fe0465d1e7a9769608dc27f70e
emirsahovic <emir.sahovic00@gmail.com>
2022-03-08 21:39:45 +0100

    API security

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ import userRoutes from './routes/userRoutes.js';
 import profileRoutes from './routes/profileRoutes.js';
 import postRoutes from './routes/postRoutes.js';
 import { errorHandler } from './middleware/errorMiddleware.js';
+import mongoSanitize from 'express-mongo-sanitize';
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import rateLimit from 'express-rate-limit';
+import hpp from 'hpp';
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
@@ -14,6 +20,19 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 mins
+    max: 100
+})
+
+app.use(limiter);
+app.use(hpp());
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
